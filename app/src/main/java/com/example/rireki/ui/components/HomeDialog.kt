cd9c23e6562011d3f04b69d47478f505ea928ee5
Erik Reisswig <erikreisswig@gmx.de>
2024-11-21 23:44:38 +0100

    UI/UX Dialog Veränderungen - Titel hat andere Typskalierung (titleLarge) und andere Farbe - TextField ist nicht mehr umrandet und enthält einen Platzhalter anstatt ein Label. Zusätzlich wird ein Icon im TextField angezeigt (ein Liste-Icon) - Die TextButtons sind jetzt nur in Großbuchstaben - Abbruch zu Abbrechen geändert - Titel geändert in "Liste hinzufügen" anstelle von "Neue Liste hinzufügen"

--- app/src/main/java/com/example/rireki/ui/components/HomeDialog.kt
+++ app/src/main/java/com/example/rireki/ui/components/HomeDialog.kt
@@ -6,10 +6,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,7 +19,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.example.rireki.R
 
@@ -43,7 +46,6 @@ fun HomeDialogAddList(
                 verticalArrangement = Arrangement.spacedBy(
                     dimensionResource(id = R.dimen.dialog_card_inner_spacing)
                 ),
-                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .padding(
                         dimensionResource(id = R.dimen.dialog_card_inner_padding)
@@ -51,7 +53,8 @@ fun HomeDialogAddList(
             ) {
                 Text(
                     text = stringResource(id = R.string.dialog_title),
-                    style = MaterialTheme.typography.headlineSmall
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onSecondaryContainer
                 )
                 HomeDialogAddListField(
                     listName = listName,
@@ -77,15 +80,22 @@ fun HomeDialogAddListField(
     Column(
         modifier = modifier
     ) {
-        OutlinedTextField(
+        TextField(
             value = listName,
             onValueChange = onValueChange,
             singleLine = true,
-            label = {
+            placeholder = {
                 Text(
                     text = stringResource(id = R.string.dialog_field_label),
                 )
-            }
+            },
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.list_fill_24),
+                    contentDescription = null
+                )
+            },
+            modifier = Modifier.padding(0.dp)
         )
     }
 }
@@ -103,15 +113,15 @@ fun HomeDialogAddListButtons(
             onClick = onDismissRequest
         ) {
             Text(
-                text = stringResource(id = R.string.dialog_cancel_button),
-                color = MaterialTheme.colorScheme.error
+                text = stringResource(id = R.string.dialog_cancel_button).uppercase(),
+                color = MaterialTheme.colorScheme.error,
             )
         }
         TextButton(
             onClick = onSubmit
         ) {
             Text(
-                text = stringResource(id = R.string.dialog_submit_button)
+                text = stringResource(id = R.string.dialog_submit_button).uppercase()
             )
         }
     }
